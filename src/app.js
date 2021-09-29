9a5929d14c5a194b568914d8917b82763288e10d
Joshua Alvarez <joshuaialvarez06@gmail.com>
2021-09-29 09:57:48 -0700

    init commit

---
+++ src/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+
+const notes = require("./data/notes-data");
+
+app.get("/notes/:noteId", (req, res, next) => {
+  const noteId = Number(req.params.noteId);
+  const foundNote = notes.find((note) => note.id === noteId);
+  if (foundNote === undefined) {
+    next(`Note id not found: ${req.params.noteId}`);
+  } else {
+    res.json({ data: foundNote });
+  }
+  
+});
+
+app.get("/notes", (req, res) => {
+  res.json({ data: notes });
+});
+
+let lastNoteId = notes.reduce((maxId, note) => Math.max(maxId, note.id), 0);
+
+// TODO: Add ability to create a new note
+app.post("/notes", (req, res, next) => {
+  const { data: { text } = {} } = req.body;
+  if (text) {
+    const newNote = {
+      id: ++lastNoteId,
+      text,
+    }
+    notes.push(newNote);
+    res.status(201).json({ data: newNote });
+  } else {
+    res.sendStatus(400);
+  }
+})
+
+// TODO: add not found handler
+app.use((req, res, next) => {
+  next(`Not found: ${req.originalUrl}`);
+})
+
+// TODO: Add error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(400).send(err);
+})
+
+module.exports = app;
